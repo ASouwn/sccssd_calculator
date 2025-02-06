2c0dfeddf33b4160e7eef2089897df661a3e9200
asouwn <asouwn@126.com>
2025-02-06 16:11:26 +0800

    first commit

---
+++ src/main/java/com/sccssd/sccssd_calculator/SccssdCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.sccssd.sccssd_calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SccssdCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SccssdCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sccssd/sccssd_calculator/SccssdCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sccssd.sccssd_calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SccssdCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
